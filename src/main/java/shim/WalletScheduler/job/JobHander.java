a7ee1d4d7b5857dfef80a408fc873c27df075ca6
duswnsxnxn <duswnsxnxn@gmail.com>
2024-08-05 17:03:59 +0900

    로깅 및 스케쥴러 종료 로직 추가

--- src/main/java/shim/WalletScheduler/job/JobHander.java
+++ src/main/java/shim/WalletScheduler/job/JobHander.java
@@ -1,6 +1,7 @@
 package shim.WalletScheduler.job;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import shim.WalletScheduler.entity.WalletQueues;
 import shim.WalletScheduler.service.WalletQueuesService;
@@ -9,12 +10,37 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JobHander {
 
     private final WalletQueuesService queueService;
+    private long startTime = System.currentTimeMillis();
+    private boolean isRunning = true;
 
     public void run() {
+
+        if (!isRunning) {
+            return;
+        }
+
+        if (startTime == 0) {
+            startTime = System.currentTimeMillis();
+            log.info("Scheduler started at: {}", startTime);
+        }
+
         List<WalletQueues> walletQueues = queueService.getWalletQueues();
+
+        if (walletQueues.isEmpty()) {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            log.info("No more data in WalletQueues. Stopping the job. Elapsed time: {} ms", elapsedTime);
+            stopScheduler();
+            return;
+        }
+
         queueService.calc(walletQueues);
+    }
+
+    private void stopScheduler() {
+        isRunning = false;
     }
 }
